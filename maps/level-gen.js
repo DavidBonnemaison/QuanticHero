4fdc819ef59dc5e9d2f818269cecd59e15efb738
David Bonnemaison <david.bonnemaison@gmail.com>
2018-02-07 22:46:13 +0000

    Add auto generated level 4

--- maps/level-gen.js
+++ maps/level-gen.js
@@ -1,13 +1,12 @@
+const fs = require('fs');
 const { random, flatten, range, sample } = require('lodash');
 
 const level = 4;
 
 const params = {
   world: {
-    width: 150,
-    // width: level * 500,
-    height: 150
-    // height: level * 1000
+    width: 1600,
+    height: 1000
   }
 };
 
@@ -15,22 +14,33 @@ class Matrix {
   constructor({ rows, cols }) {
     this.rows = rows;
     this.cols = cols;
-    this.rowSpacing = 1;
-    this.colSpacing = 1;
+    this.rowSpacing = 8;
+    this.colSpacing = 8;
     this.cells = range(rows).map(row => range(cols).map(col => ' '));
     this.platforms = [];
     this.reachable = {
-      top: 2,
-      side: 3,
-      bottom: 3
+      top: 8,
+      side: 40,
+      bottom: 40
     };
+  }
+
+  getCoordinates() {
+    return this.platforms.map(({ x, y, width, height }) => {
+      return {
+        x: x * 10,
+        y: y * 10,
+        width: width * 10,
+        height: height * 10
+      };
+    });
   }
 
   setCell({ row, col, content }) {
     this.cells[row][col] = content;
   }
 
-  insertPlatform({ x = 0, y = 0, width = 2, height = 1 }) {
+  insertPlatform({ x = 0, y = 0, width = 8, height = 1 }) {
     range(width).forEach(w =>
       range(height).forEach(h => this.setCell({ row: y + h, col: x + w, content: '_' }))
     );
@@ -39,8 +49,8 @@ class Matrix {
 
   insertIfAvailable({ x, y, width, height }) {
     if (
-      x + width > this.cols ||
-      y + height > this.rows ||
+      x + width + this.colSpacing > this.cols ||
+      y + height + this.rowSpacing > this.rows ||
       x < this.colSpacing ||
       y < this.rowSpacing
     ) {
@@ -67,9 +77,12 @@ class Matrix {
 
   nextPlatform() {
     const platform = sample(this.platforms);
-    const x = random(platform.x - this.reachable.side, platform.x + this.reachable.side);
+    const x = random(
+      platform.x - this.reachable.side,
+      platform.x + platform.width + this.reachable.side
+    );
     const y = random(platform.y - this.reachable.bottom, platform.y + this.reachable.top);
-    const width = 3;
+    const width = 12;
     const height = 1;
     this.insertIfAvailable({
       x,
@@ -81,8 +94,9 @@ class Matrix {
 }
 
 const matrix = new Matrix({ rows: params.world.width / 10, cols: params.world.height / 10 });
-matrix.insertPlatform({ x: 3, y: 13, width: 9, height: 2 });
-range(30).forEach(() => {
+matrix.insertPlatform({ x: 40, y: 99, width: 80, height: 1 });
+range(10000).forEach(() => {
   matrix.nextPlatform();
 });
-console.log(matrix.cells);
+
+fs.writeFileSync('maps/gen.json', JSON.stringify(matrix.getCoordinates()));
