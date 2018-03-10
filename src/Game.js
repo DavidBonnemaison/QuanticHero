91045f4c323f8d253e5ff01362c33543623a44e8
David Bonnemaison <david.bonnemaison@gmail.com>
2018-03-10 18:16:50 +0000

    Setup cordova

--- src/Game.js
+++ src/Game.js
@@ -27,6 +27,28 @@ class Game extends Phaser.Game {
     if (!window.cordova) {
       this.state.start('Boot');
     }
+    if (window.cordova) {
+      var app = {
+        initialize: function() {
+          document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        },
+
+        // deviceready Event Handler
+        //
+        onDeviceReady: function() {
+          this.receivedEvent('deviceready');
+
+          // When the device is ready, start Phaser Boot state.
+          window.game.state.start('Boot');
+        },
+
+        receivedEvent: function(id) {
+          console.log('Received Event: ' + id);
+        }
+      };
+
+      app.initialize();
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,26 +1,3 @@
 import './App';
 
 // window.game = new Game();
-
-if (window.cordova) {
-  var app = {
-    initialize: function() {
-      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    onDeviceReady: function() {
-      this.receivedEvent('deviceready');
-
-      // When the device is ready, start Phaser Boot state.
-      window.game.state.start('Boot');
-    },
-
-    receivedEvent: function(id) {
-      console.log('Received Event: ' + id);
-    }
-  };
-
-  app.initialize();
-}
